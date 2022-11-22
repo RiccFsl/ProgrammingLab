91b8e2039dca1f262f891d699e86e52c1c09885f
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2022-11-22 14:55:28 +0000

    primo oggetto

--- LI/oggetti.py
+++ LI/oggetti.py
@@ -6,13 +6,12 @@ class CSVFile():
   def get_data(nome_file):
     
     values = []
-  my_file = open(nome_file, 'r')
-  for line in my_file:
-    element = line.split(',')
-    if element[0] != 'Date':
-      value = element[1]
-      values.append(float(value))
-  return values
-  my_file.close()
+    my_file = open(nome_file, 'r')
+    for line in my_file:
+      element = line.split(',')
+      if element[0] != 'Date':
+        value = element[1]
+    my_file.close()
+    return values
 
 my_file = CSVFile('shampoo_sales.txt')
