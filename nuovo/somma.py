1f4022afcba32a76512bc354380f555ad6ad39fc
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-01-18 14:41:11 +0000

    .

--- nuovo/somma.py
+++ nuovo/somma.py
@@ -1,6 +1,6 @@
 def sum_csv(file_name):
     list = []
-    my_file = open('shampoo_sales.txt', 'r')
+    my_file = open('shampoo_sales.txt', r)
     for line in my_file:
         my_file.split(',')
         if (element[0] != 'Date'):
