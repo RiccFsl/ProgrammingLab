b07498934df6404153b1b15f7146142fc59a3d65
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-01-18 16:43:36 +0000

    - ""

--- nuova/somma.py
+++ nuova/somma.py
@@ -1,6 +1,6 @@
 def sum_csv(file_name):
     list = []
-    my_file = open('file_name', 'r')
+    my_file = open(file_name, 'r')
     for line in my_file:
         my_file = line.split(',')
         if (element[0] != 'Date'):
