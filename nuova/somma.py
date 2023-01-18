dda80c9019f7d03545544fdf6b95ccb78ffd7e24
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-01-18 16:57:15 +0000

    open file fuori dalla funzione + return

--- nuova/somma.py
+++ nuova/somma.py
@@ -1,9 +1,8 @@
 def sum_csv(file_name):
     list = []
-    my_file = open(file_name, 'r')
     for line in my_file:
         my_file = line.split(',')
         if (element[0] != 'Date'):
             value = float(element[1])
             list.append(value)
-    my_file.close()
+    return list
