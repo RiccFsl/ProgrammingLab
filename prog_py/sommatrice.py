e4b544cd5b758e6d831356dd0bd5705a0af9a84c
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-01-18 15:25:53 +0100

    Delete prog_py directory

--- prog_py/sommatrice.py
+++
@@ -1,5 +0,0 @@
-def sommatrice(list):
-    return sum(list)
-
-my_list = [1,2,3,4,5,6,7,8,9]
-print('sum = {}'.format(sommatrice(my_list)))
--- prog_py/zip.py
+++
@@ -1,9 +0,0 @@
-def prova (l1, l2):
-  for i in zip(l1,l2):
-    print(i)
-  
-list1 = [1,2,3]
-list2 = [4,5,6]
-prova (list1, list2)
-  
-  
