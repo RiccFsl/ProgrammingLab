e64d54a1e3fddcbc3bd32c89e47bcf5a1ddde499
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-01-18 14:46:27 +0000

    aa

--- nuovo/somma.py
+++ nuovo/somma.py
@@ -1,8 +1,9 @@
 def sum_csv(file_name):
     list = []
-    my_file = open('shampoo_sales.txt', r)
+    my_file = open('file_name', 'r')
     for line in my_file:
-        my_file.split(',')
+        my_file = line.split(',')
         if (element[0] != 'Date'):
             value = float(element[1])
             list.append(value)
+    my_file.close()
