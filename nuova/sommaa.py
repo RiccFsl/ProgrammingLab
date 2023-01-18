7dff3370808550f769449570a6145e217db53f4a
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-01-18 16:30:17 +0000

    deleting

--- nuova/sommaa.py
+++
@@ -1,9 +0,0 @@
-def sum_csv(file_name):
-    list = []
-    my_file = open('file_name', 'r')
-    for line in my_file:
-        my_file = line.split(',')
-        if (element[0] != 'Date'):
-            value = float(element[1])
-            list.append(value)
-    my_file.close()
