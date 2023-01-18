9b63001da86095d84a73715466e16aee4185581d
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-01-18 17:01:27 +0000

    nomi variabili

--- nuova/somma.py
+++ nuova/somma.py
@@ -1,7 +1,7 @@
 def sum_csv(file_name):
     list = []
-    for line in my_file:
-        my_file = line.split(',')
+    for line in file_name:
+        file_name = line.split(',')
         if (element[0] != 'Date'):
             value = float(element[1])
             list.append(value)
