c323247f71ff62e3be279d844cc86a5a14e79e48
RiccFsl <115220962+RiccFsl@users.noreply.github.com>
2023-02-12 17:08:14 +0000

    nuova lezione

---
+++ LI/classe1.py
@@ -0,0 +1,14 @@
+class CSVFile:
+	def __init__(self, name):
+		self.name = name
+		self.my_file = open(self.name, "r")
+		
+	def get_data (self):
+		dati = []
+		next(self.my_file)
+		for line in self.my_file:
+			element = line.split(",")
+			Data = element[0]
+			Value = element[1].strip()
+			dati.append((Data, float(Value),))
+		return dati
--- nuova/somma.py
+++
@@ -1,9 +0,0 @@
-def sum_csv(file_name):
-    list = []
-    my_file = open(file_name , 'r')
-    for line in my_file:
-        element = line.split(',')
-        if (element[0] != 'Date'):
-            value = (element[1])
-            list.append(float(value))
-    return sum(list)
